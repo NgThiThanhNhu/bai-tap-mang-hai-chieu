19e70b2a3ae427aab86c2a8c4295fce15ff09194
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2026-03-31 09:40:06 +0700

    add bai 1 va gitignore

---
+++ Bai1/Bai1/Program.cs
@@ -0,0 +1,22 @@
+﻿int[,] arr1 = new int[3, 3];
+Console.Write("\nNhap mang hai chieu ------\n");
+for (int i=0; i<3; i++)
+{
+    for (int j=0; j<3; j++)
+    {
+        Console.Write("\nNhap phan tu - [{0}, {1}]: ", i, j);
+        arr1[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.WriteLine("\nDuyet mang-----------");
+for (int i=0; i<3; i++)
+{
+    Console.Write("\n");
+    for (int j=0; j<3; j++)
+    {
+        Console.Write($"{arr1[i, j]}\t");
+    }
+}
+
+Console.Write("\n\n");
